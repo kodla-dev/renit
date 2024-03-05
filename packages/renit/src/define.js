afa6bd56b6e9719cc846c287922662f0d6de4410
Mehmet Seyfi <mehmetcanseyfi@gmail.com>
2024-03-05 21:35:46 +0300

    Add new functions to 'is' library

--- packages/renit/src/define.js
+++ packages/renit/src/define.js
@@ -6,10 +6,39 @@
 */
 
 /**
+ * Represents the maximum safe integer.
+ * @type {number}
+ */
+export const MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER || Math.pow(2, 53) - 1;
+
+/**
+ * Used to indicate asynchronous behavior.
+ * @type {string}
+ */
+export const RAW_ASYNC = 'async';
+
+/**
+ * Represents async functions.
+ * @type {string}
+ */
+export const RAW_ASYNC_FUNCTION = 'AsyncFunction';
+
+/**
  * Represents boolean values.
  * @type {string}
  */
 export const RAW_BOOLEAN = 'boolean';
+
+/**
+ * Represents the "class" expression in object-oriented programming.
+ * @type {string}
+ */
+export const RAW_CLASS = 'class';
+
+/**
+ * The name of the class attribute used in DOM manipulation.
+ */
+export const RAW_CLASS_NAME = 'className';
 
 /**
  * Represents the function expression.
@@ -18,7 +47,67 @@ export const RAW_BOOLEAN = 'boolean';
 export const RAW_FUNCTION = 'function';
 
 /**
- * Represents async functions.
+ * Represents the "Map" data structure.
  * @type {string}
  */
-export const RAW_ASYNC_FUNCTION = 'AsyncFunction';
+export const RAW_MAP = 'map';
+
+/**
+ * Represents a non-numeric value.
+ * @type {string}
+ */
+export const RAW_NAN = 'NaN';
+
+/**
+ * Represents the null value.
+ * @type {string}
+ */
+export const RAW_NULL = 'null';
+
+/**
+ * Represents numerical values.
+ * @type {string}
+ */
+export const RAW_NUMBER = 'number';
+
+/**
+ * Represents objects.
+ * @type {string}
+ */
+export const RAW_OBJECT = 'object';
+
+/**
+ * Represents asynchronous operations.
+ * @type {string}
+ */
+export const RAW_PROMISE = 'promise';
+
+/**
+ * Represents the data structure for storing unique values.
+ * @type {string}
+ */
+export const RAW_SET = 'set';
+
+/**
+ * Represents text values.
+ * @type {string}
+ */
+export const RAW_STRING = 'string';
+
+/**
+ * The name of the style attribute used in DOM manipulation.
+ * @type {string}
+ */
+export const RAW_STYLE = 'style';
+
+/**
+ * Represents unique values.
+ * @type {string}
+ */
+export const RAW_SYMBOL = 'symbol';
+
+/**
+ * Represents the undefined value.
+ * @type {string}
+ */
+export const RAW_UNDEFINED = 'undefined';
--- packages/renit/src/type.d.ts
+++ packages/renit/src/type.d.ts
@@ -1,18 +1,32 @@
 /**
- * Type that checks if type `R` extends type `N`, and returns type `R` if true,
- * otherwise returns `never`.
+ * Type representing an array or a readonly array.
  *
- * @template R - The type to be checked.
- * @template N - The type to check against.
+ * @template R - The type of elements in the array.
  */
-export type Include<R, N> = R extends N ? R : never;
+export type $array<R = unknown> = R[] | Readonly<R[]>;
 
 /**
- * Alias representing a readonly array.
+ * Type representing either an array or an object literal with keys of type
+ * string or number, and values of type R or an array of R.
  *
- * @template R - The type of elements in the list.
+ * @template R - The type of values in the array or object literal.
  */
-export type List<R = any> = ReadonlyArray<R>;
+export type $collect<R = unknown> = $array<R> | $object<R>;
+
+/**
+ * Type representing an object literal with keys of type string or number, and
+ * values of type R or an array of R.
+ *
+ * @template R - The type of values in the object literal.
+ */
+export type $object<R = unknown> = ObjectLiteral<R>;
+
+/**
+ * Represents a type that behaves like an array by having a 'length' property.
+ */
+export interface ArrayLikeLiteral {
+  readonly length: number;
+}
 
 /**
  * Alias to create a function.
@@ -29,3 +43,45 @@ export type Arrow<R extends List = any, N extends any = any> = (...args: R) => N
  * @template N - The return type.
  */
 export type AsyncArrow<R extends List = any[], N = any> = Arrow<R, Promise<N>>;
+
+/**
+ * Type representing a class constructor that takes arguments of type R and
+ * creates an instance of type N.
+ *
+ * @template R - The type of arguments the class constructor takes.
+ * @template N - The type of the class instance.
+ */
+export type ClassLiteral<R = any, N = any> = new (...args: R[]) => N;
+
+/**
+ * Type that checks if type `R` extends type `N`, and returns type `R` if true,
+ * otherwise returns `never`.
+ *
+ * @template R - The type to be checked.
+ * @template N - The type to check against.
+ */
+export type Include<R, N> = R extends N ? R : never;
+
+/**
+ * Alias representing a readonly array.
+ *
+ * @template R - The type of elements in the list.
+ */
+export type List<R = any> = ReadonlyArray<R>;
+
+/**
+ * Interface representing an object literal with keys of type string or number,
+ * and values of type R or an array of R.
+ *
+ * @template R - The type of values in the object literal.
+ */
+export interface ObjectLiteral<R> {
+  [key: string | number]: R | R[];
+}
+
+/**
+ * Extract the return type of a function.
+ *
+ * @template R - The arrow function type.
+ */
+export type Return<R extends Arrow> = R extends (...args: List) => infer N ? N : never;
