98ad9daed7dcccfbb7fd48f7c68fb67109a3ba0c
Mehmet Seyfi <mehmetcanseyfi@gmail.com>
2024-03-07 18:21:36 +0300

    Add toStringify helper

---
+++ packages/renit/src/to.js
@@ -0,0 +1,20 @@
+/**
+  Data Transformation
+  ------------------------------------------------------------------------------
+  It converts the types or values of variables or constants.
+  ------------------------------------------------------------------------------
+*/
+
+import { isObject, isUndefined } from './is.js';
+
+/**
+ * Converts the specified collection to a string representation.
+ *
+ * @param {*} [collect] - The collection to convert to a string.
+ * @returns {*} Returns the string representation of the collection.
+ */
+export function toStringify(collect) {
+  if (isUndefined(collect)) return collect => toStringify(collect);
+  if (isObject(collect)) return JSON.stringify(collect);
+  return `${collect}`;
+}
