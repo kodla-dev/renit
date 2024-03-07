75c97a9a23df88437e100ed000854ce20b9cbde0
Mehmet Seyfi <mehmetcanseyfi@gmail.com>
2024-03-07 21:03:48 +0300

    Add pipe and clone functions

---
+++ packages/renit/src/helper.js
@@ -0,0 +1,34 @@
+import { reduce } from './collect.js';
+import { DEVELOPMENT } from './define.js';
+import { Renit } from './fault.js';
+import { isArray, isFunction, isPromise } from './is.js';
+
+/**
+ * It's a structure that connects functions by passing the output of one function as the arguments
+ * to another function.
+ *
+ * @param {Array|Object|Promise|Function} collect - The collection to pipe.
+ * @param {...Function} fns - The functions to pipe the collection through.
+ * @returns {*} - Returns the result of piping the collection through the functions.
+ * @throws {Renit} - Throws a Renit error if in development mode.
+ */
+export function pipe(collect, ...fns) {
+  if (isFunction(collect)) return c => pipe(c, [collect, ...fns]);
+  return reduce(run, collect, fns);
+}
+
+function run(a, f) {
+  if (isFunction(f)) return isPromise(a) ? a.then(f) : f(a);
+  if (isArray(f)) return pipe(a, ...f);
+  if (DEVELOPMENT) throw new Renit("Type error in 'pipe' function");
+}
+
+/**
+ * Creates a deep copy of a collection using the structured cloning algorithm.
+ *
+ * @param {Array|Object} collect - The collection to clone.
+ * @returns {Array|Object} - Returns the cloned collection.
+ */
+export function clone(collect) {
+  return structuredClone(collect);
+}
