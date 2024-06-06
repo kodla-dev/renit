3f74bd1147c592a8ee04dbf8c91fcd763f86696b
Mehmet Seyfi <mehmetcanseyfi@gmail.com>
2024-06-06 16:44:07 +0300

    add, avg, size, and sum helpers

--- packages/renit/src/libraries/math/index.js
+++ packages/renit/src/libraries/math/index.js
@@ -1,5 +1,46 @@
-import { keys } from '../collect/index.js';
-import { isArrayLike, isObject, isPromise, isUndefined } from '../is/index.js';
+import { keys, map, pluck, reduce } from '../collect/index.js';
+import {
+  isArray,
+  isArrayLike,
+  isCollect,
+  isFunction,
+  isNumber,
+  isObject,
+  isPromise,
+  isString,
+  isUndefined,
+} from '../is/index.js';
+
+/**
+ * Adds two values together. Supports numbers, strings, and collections.
+ *
+ * @param {number|string} a - The first value to add.
+ * @param {number|string|Array} [b] - The second value to add or a collection to map.
+ * @returns {Function|number|string|Array} The sum of the values, a function if only one value is provided, or a mapped collection.
+ */
+export function add(a, b) {
+  if (isUndefined(b)) return b => add(a, b);
+  if ((isNumber(a) && isNumber(b)) || (isString(a) && isString(b))) return a + b;
+  if (isCollect(b)) return map(add(a), b);
+}
+
+/**
+ * Calculates the average of a collection. Optionally averages the values of a specified key or the
+ * result of a function applied to each element.
+ *
+ * @param {string|Function} [key] - The key to pluck or the function to map over the collection.
+ * @param {Array} [collect] - The collection to average.
+ * @returns {number} The average of the collection.
+ */
+export function avg(key, collect) {
+  if (isUndefined(collect)) {
+    if (isUndefined(key)) return collect => avg(collect);
+    else if (isString(key) || isFunction(key)) return collect => avg(key, collect);
+    return avg(void 0, key);
+  }
+
+  return sum(key, collect) / size(collect);
+}
 
 /**
  * Returns the size of the specified collection.
@@ -25,4 +66,34 @@ export function size(collect) {
  */
 function length(collect) {
   return collect.length;
+}
+
+/**
+ * Calculates the sum of a collection. Optionally sums the values of a specified key or the result
+ * of a function applied to each element.
+ *
+ * @param {string|Function} [key] - The key to pluck or the function to map over the collection.
+ * @param {Array} [collect] - The collection to sum.
+ * @returns {number} The sum of the collection.
+ */
+export function sum(key, collect) {
+  if (isUndefined(collect)) {
+    if (isUndefined(key)) return collect => sum(collect);
+    else if (isString(key) || isFunction(key)) return collect => sum(key, collect);
+    return sum(void 0, key);
+  }
+
+  let total = 0;
+
+  if (isArray(collect)) {
+    // Sum the collection directly if no key is provided
+    if (isUndefined(key)) total = reduce(add, collect);
+    // Pluck the values of the specified key and sum them
+    else if (isString(key)) total = sum(pluck(key, collect));
+    // Map the function over the collection and sum the results
+    else if (isFunction(key)) total = sum(map(key, collect));
+  }
+
+  // Return the sum with precision
+  return parseFloat(total.toPrecision(12));
 }
