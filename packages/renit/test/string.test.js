f9f354b8f2eb02ef71ccd7407dbd31dc89949da6
Mehmet Seyfi <mehmetcanseyfi@gmail.com>
2024-06-10 14:37:56 +0300

    Add string library

---
+++ packages/renit/test/string.test.js
@@ -0,0 +1,60 @@
+import { describe, expect, it } from 'vitest';
+import { lower, sub, upper, words } from '../src/libraries/string/index.js';
+
+describe('lower', () => {
+  it('lower:text', () => {
+    expect(lower('LOREM')).toEqual('lorem');
+  });
+});
+
+describe('sub', () => {
+  it('sub:text', () => {
+    expect(sub('Lorem')).toEqual('orem');
+  });
+
+  it('sub:start', () => {
+    expect(sub(0, 'Lorem')).toEqual('Lorem');
+  });
+
+  it('sub:end', () => {
+    expect(sub(0, 1, 'Lorem')).toEqual('L');
+  });
+});
+
+describe('upper', () => {
+  it('upper:text', () => {
+    expect(upper('lorem')).toEqual('LOREM');
+  });
+});
+
+describe('words', () => {
+  it('words:true', () => {
+    const result = words('Lorem ipsum\ndolor sit amet, consectetur adipiscing\telit.');
+    const expected = [
+      'Lorem',
+      'ipsum',
+      'dolor',
+      'sit',
+      'amet',
+      'consectetur',
+      'adipiscing',
+      'elit',
+    ];
+    expect(result).toEqual(expected);
+  });
+
+  it('words:false', () => {
+    const result = words(false, 'Lorem ipsum\ndolor sit amet, consectetur adipiscing\telit.');
+    const expected = [
+      'Lorem',
+      'ipsum',
+      'dolor',
+      'sit',
+      'amet,',
+      'consectetur',
+      'adipiscing',
+      'elit.',
+    ];
+    expect(result).toEqual(expected);
+  });
+});
